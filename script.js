d3a42348ef80ccc7ce3f0660ab7d1ac1c4a4b9b8
mark <122426238+dobladamark@users.noreply.github.com>
2025-08-19 22:11:08 +0800

    Update script.js

--- script.js
+++ script.js
@@ -877,3 +877,15 @@ console.log(charArray); // ["o", "l", "l", "e", "h"]
 let reversedArray = ["o", "l", "l", "e", "h"];
 let reversedString = reversedArray.join("");
 console.log(reversedString); // "olleh"
+
+array.indexOf(element, fromIndex)
+let fruits = ["apple", "banana", "orange", "banana"];
+let index = fruits.indexOf("banana");
+console.log(index); // 1
+
+let fruits = ["apple", "banana", "orange"];
+let index = fruits.indexOf("grape");
+console.log(index); // -1
+let colors = ["red", "green", "blue", "yellow", "green"];
+let index = colors.indexOf("green", 3);
+console.log(index); // 4
